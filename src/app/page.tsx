7466d7d963c40d802f7f08f0e7f5e9fba0d9e828
Hamma Anas <anesjob70@example.com>
2025-07-11 00:34:00 +0100

    almost there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,70 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative w-full min-h-screen text-white">
+      {/* Fixed Background */}
+      <div className="fixed inset-0 -z-10 bg-scroll" />
+      <nav className="mx-3 flex justify-evenly">
+      <button>Self-Introduction</button>
+      <button>My Experince</button>
+      <button>My Projects</button>
+      </nav>
+      <div className="flex justify-center">
+      {/* Main content (scrollable) */}
+      <div className="px-8 py-16 flex flex-col gap-y-16">
+        {/* HERO SECTION */}
+        <div className="flex justify-evenly gap-x-20 flex-wrap">
+          <div className="max-w-[300px]">
+            <img src="/me.jpg" alt="anas" className="rounded-xl w-full" />
+          </div>
+          <div className="self-center">
+            <h1 className="font-bold text-3xl">
+              Welcome to <span className="text-red-200">Anas</span>
+            </h1>
+            <h1 className="font-bold text-3xl">
+              <span className="text-green-200">Hamma</span>'s Portfolio
+            </h1>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* EXPERIENCE SECTION */}
+        
+        <h2 className="text-2xl font-bold">Experience</h2>
+        <div className="h-[400px] bg-white/10 rounded-xl p-4 flex gap-7">
+          <div className="max-w-[300px]">
+            <img src="/host_anas.jpg" alt="anas" className="rounded-xl w-full"/>
+          </div>
+          <div className="self-center">
+            <div className="mb-5">
+            <h2 className="font-bold">NCS Vice-President</h2>
+            <h1>
+              I actively contributed to the management and
+              management of club activities. <br /> I contributed to the organization
+              events such as hackathons, workshops,<br /> and networking sessions for students
+              passionate about IT.  
+            </h1>
+            </div>
+            <div>
+            <h2 className="font-bold">Internship in AI UNIVERSE</h2>
+            <h1>
+              During my time at an AI startup, I gained hands-on experience working <br /> with advanced AI technologies, particularly Retrieval-Augmented Generation (RAG) systems <br /> and AI agents.
+              I was involved in designing and optimizing pipelines that combined language <br /> models  with external data sources to deliver more accurate and context-aware responses. 
+            </h1>
+            </div>
+          </div>
+        </div>
+
+        {/* PROJECTS SECTION */}
+        <h2 className="text-2xl font-bold mb-2">Projects</h2>
+        <div className="h-[400px] bg-white/10 rounded-xl p-6">
+          
+        </div>
+        
+        {/* FOOTER */}
+        <footer className="pt-10 text-center text-sm text-white/70">
+          © 2025 Anas Hamma
+        </footer>
+        </div>
+      </div>
     </div>
   );
 }
